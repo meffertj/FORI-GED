1d5c193407faa7f153ef87f91fcf279434a25b62
d-hash <andreadasc96@gmail.com>
2025-03-03 12:32:33 +0100

    removed old imdb scripts

--- scripts/testing_imdb_10.sh
+++
@@ -1,9 +0,0 @@
-for g in 666
-do
-for h in 83 87 474 511 525 854
-do
-./gurobi_imdb -f F2 -g ${g} -h ${h}  -t 8 -l 600 -w F2/IMDB/IMDB_10
-./gurobi_imdb -f F2+ -g ${g} -h ${h}  -t 8 -l 600 -w F2Plus/IMDB/IMDB_10
-./gurobi_imdb -f FORI -g ${g} -h ${h}  -t 8 -l 600 -w FORI/IMDB/IMDB_10
-done
-done
--- scripts/testing_imdb_30.sh
+++
@@ -1,9 +0,0 @@
-for g in 666
-do
-for h in 272 775 806 900 1290 1426
-do
-./gurobi_imdb -f F2 -g ${g} -h ${h}  -t 8 -l 600 -w F2/IMDB/IMDB_30
-./gurobi_imdb -f F2+ -g ${g} -h ${h}  -t 8 -l 600 -w F2Plus/IMDB/IMDB_30
-./gurobi_imdb -f FORI -g ${g} -h ${h}  -t 8 -l 600 -w FORI/IMDB/IMDB_30
-done
-done
